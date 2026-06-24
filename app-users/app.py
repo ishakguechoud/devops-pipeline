3cc1f8c3ebf0de80eb3455cb1bcdd17c427d7650
zakboob <ishakguechoud.10@gmail.com>
2026-06-24 10:30:43 +0100

    ajoute base de donnes

--- app-users/app.py
+++ app-users/app.py
@@ -1,27 +1,54 @@
 from flask import Flask, jsonify
+import psycopg2
 
 app = Flask(__name__)
 
-users = [
-    {"id": 1, "name": "Izak", "email": "izak@amana.dz"},
-    {"id": 2, "name": "Belakcem", "email": "belkacem@gmail.com"},
-    {"id": 3, "name": "Athman", "email": "athman@gmail.com"}
-]
+DB_HOST = "192.168.74.129"
+DB_NAME = "workflow"
+DB_USER = "workflow_app"
+DB_PASSWORD = "Guizak-56"
+
+def get_connection():
+    return psycopg2.connect(
+        host=DB_HOST,
+        database=DB_NAME,
+        user=DB_USER,
+        password=DB_PASSWORD
+    )
 
 @app.route('/health')
 def health():
-    return jsonify({"status": "ok", "service": "app-users"})
+    return jsonify({
+        "status": "ok",
+        "service": "app-users"
+    })
 
 @app.route('/users')
 def get_users():
-    return jsonify({"users": users})
 
-@app.route('/users/<int:user_id>')
-def get_user(user_id):
-    user = next((u for u in users if u["id"] == user_id), None)
-    if user:
-        return jsonify(user)
-    return jsonify({"error": "User not found"}), 404
+    conn = get_connection()
+    cur = conn.cursor()
+
+    cur.execute("""
+        SELECT id, username, email
+        FROM users
+    """)
+
+    rows = cur.fetchall()
+
+    users = []
+
+    for row in rows:
+        users.append({
+            "id": row[0],
+            "username": row[1],
+            "email": row[2]
+        })
+
+    cur.close()
+    conn.close()
+
+    return jsonify({"users": users})
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5001)
